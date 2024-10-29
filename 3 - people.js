15a9637c99bb22133df2b491a919c821833d7273
alexandru233847 <alexutualexino33@gmail.com>
2024-10-29 18:53:28 +0100

    Create a function

--- 3 - people.js
+++ 3 - people.js
@@ -1,1 +1,19 @@
-const createName = 
+const createName = require('./1 - names');
+const createHobbies = require('./2 - hobbies');
+
+// Create a function
+function createPerson() {
+    const name = createName('Simone', 'Carmello');
+    const hobbies = createHobbies('Reading', 'Traveling', 'Cooking');
+
+    return {
+        firstName: name.firstName,
+        lastName: name.lastName,
+        hobbies: hobbies.hobbies
+    };
+}
+
+module.exports = createPerson;
+
+const person = createPerson();
+console.log(person);
